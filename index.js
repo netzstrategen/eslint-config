b13d1801172ff341f756eabea2e20fd989174197
Daniel Kudwien <daniel@netzstrategen.com>
2021-08-26 17:25:59 +0200

    Updated with rules from HAUR, added prettier.

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ module.exports = {
     ecmaVersion: 2021,
     sourceType: 'module',
   },
-  extends: ['airbnb-base'],
-  plugins: ['import'],
+  extends: ['eslint:recommended', 'google'],
+  plugins: ['import', 'prettier'],
   rules: {
     'comma-dangle': ['error', 'only-multiline'],
     'import/no-cycle': [0, { ignoreExternal: true }],
     quotes: [2, 'single', { avoidEscape: true }],
+    'prettier/prettier': ['error', { singleQuote: true }],
   },
 };
